9b5ff7c87dd02173fa58b3d28810d09f821474aa
Nina Tuluptseva <p.pin.p@yandex.ru>
2017-04-10 15:08:24 +0300

    Begin work with keras

---
+++ fromMarkingToDir.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import sys
+import hashlib
+import base64
+import cv2
+import numpy as np
+from libs.marking import Marking
+from libs.imageProcess import cropByRect
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('marking_file', type=str, nargs='?', help='input marking file')
+parser.add_argument('outdir', type=str, nargs='?', help='dir to ouput')
+
+frame_dir = '/media/nina/Seagate Backup Plus Drive/hockey/frames'
+masks_dir = '/media/nina/Seagate Backup Plus Drive/hockey/masks'
+
+args = parser.parse_args()
+
+def md5_hash(arr):
+    md5 = hashlib.md5() 
+    arr = base64.b64encode(arr)
+    md5.update(arr)
+    return md5.hexdigest()
+
+marking = Marking()
+with open(args.marking_file) as fin:
+    marking.addFromJson(fin)
+outdir = args.outdir
+
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+
+for n in range(1, 99):
+    dirname = '{}'.format(n)
+    dirname = os.path.join(outdir, dirname)
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+
+for name, objs  in marking.getByMark('number').items():
+    frame = cv2.imread(os.path.join(frame_dir, name))
+    for obj in objs:
+        mark = obj['number']
+        rect = obj['body_rect']
+        img = cropByRect(frame, rect)
+        img = np.array(img)
+        dirname = os.path.join(outdir, mark)
+        outpath = os.path.join(dirname, '{}.png'.format(md5_hash(img)))
+        cv2.imwrite(outpath, img)
+
+
+
+
+
+
+
+
---
+++ libs/imageProcess.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+
+def getOrientation(bin_image, union_all=False):
+    _, contours, hierarchy = cv2.findContours(bin_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if union_all:
+        cnt = np.concatenate(contours, axis=0)
+    else:
+        cnt = contours[0]
+    ellipse = cv2.fitEllipse(cnt)
+    center, size, angle = ellipse
+    if angle > 90:
+        angle = angle - 180
+    """
+    if -45 <= angle < 45:
+        return center, size, angle
+    elif 45 <= angle < 135:
+        angle = -90 + angle
+        size = size[1], size[0]
+    elif 135 <= angle:
+        angle = -180 + angle
+    elif -135 <= angle < -45:
+        angle = angle + 90
+        size = size[1], size[0]
+    elif angle < -135:
+        angle = 180 + angle
+    """
+    return center, size, angle
+
+
+def getBoundingBox(bin_image, union_all=False):
+    _, contours, hierarchy = cv2.findContours((bin_image * 1).astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if union_all:
+        cnt = np.concatenate(contours, axis=0)
+    else:
+        cnt = contours[0]
+    return cv2.boundingRect(cnt)
+
+
+def scaleImage(image, scale):
+    rot_matrix = cv2.getRotationMatrix2D((image.shape[1] / 2.0, image.shape[0] / 2.0), 0, scale)
+    rows = image.shape[0]
+    cols = image.shape[1]
+    image = cv2.warpAffine(image, rot_matrix, (cols, rows))
+    return image
+
+
+def cropByMask(image, mask, center):
+    n_x, n_y, n_w, n_h = getBoundingBox(mask, True)
+    mask = mask[n_y:n_y + n_h, :][:, n_x:n_x + n_w]
+    image = image[n_y:n_y + n_h, :][:, n_x:n_x + n_w]
+    center = center[0] - n_x, center[1] - n_y
+    return image, mask, center, (n_w, n_h)
+
+def cropByRect(frame, rect):
+    x, y, w, h = map(int, [rect["x"], rect["y"], rect["w"],rect["h"]])
+    return frame[y:y+h, x:x+w]
+
+def bgr2lab(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
+
+def lab2bgr(img):
+    return cv2.cvtColor(img, cv2.COLOR_Lab2BGR)
+
+def bgr2rgb(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def rgb2bgr(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+def bgr2hsv(img):
+    return cv2.cvtColor(img,  cv2.COLOR_BGR2HSV)
+
+def hsv2rgb(img):
+    return cv2.cvtColor(img,  cv2.COLOR_HSV2RGB)
+    
+def hsv2bgr(img):
+    return cv2.cvtColor(img,  cv2.COLOR_HSV2BGR)
+
+def morphologyClosing(binaryImg):
+    return cv2.morphologyEx(binaryImg, cv2.MORPH_CLOSE, np.ones((3, 3)))
+
+
+
